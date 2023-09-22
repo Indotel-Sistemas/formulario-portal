c708f80ba6407d8b7b4cd2a487dd40ef6cb557b9
Eduardo Hidalgo <eduardohidalgo0210@gmail.com>
2023-09-22 09:41:44 -0400

    first commit dev

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -3,10 +3,11 @@ import React from 'react'
 export const LoginForm = () => {
   return (
     <>
-    <div></div>
+    <div  className='login_container'>
     <div className='login_form'>
       <p className='auth_title' >Bienvenido</p>
 
+    </div>
     </div>
 
     </>
