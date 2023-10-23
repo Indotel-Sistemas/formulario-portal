feae521f753fe4963b5375a6e31c4e00e7b9bfcf
Antony Ventura Morey <aventura@indotel.gob.do>
2023-10-23 10:01:00 -0300

    api apilin updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import { AppRouter } from './routers/AppRouter';
 
 // export const source = 'http://localhost:8066';
-export const source = 'http://192.168.100.61:8067'; //server production local
+// export const source = 'http://192.168.100.61:8067'; //server production local
 // const source = 'http://192.168.100.60:8066'; //server desarrollo local
+// export const source = 'https://apilin.indotel.gob.do:8067'; //server production local
+
+export const source = 'https://10.79.30.5:8067'; //server production local
 
 export const App = () => {
   return (
